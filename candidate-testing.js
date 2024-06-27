1983827d21fc44c95e36d7e25039daa480a1de15
Nicholas Cerame <nickcerame@Nicholass-MacBook-Air.local>
2024-06-27 08:22:23 -0500

    Added in multiple question functionality + minor formatting

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,14 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -24,16 +29,30 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  //candidateAnswer = input.question(question);
+
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i]));
+    console.log();
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-  if (candidateAnswer === correctAnswer) {
+  /*if (candidateAnswer === correctAnswer) {
     console.log("You've entered the correct answer!")
   } else {
     console.log("Incorrect!")
+  }*/
+
+  console.log(`Candidate Name: ${candidateName}`);
+
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    console.log(`${i + 1}) ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}`);
+    console.log();
   }
 
 
@@ -47,6 +66,7 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log("Hello,", candidateName);
+   console.log();
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
